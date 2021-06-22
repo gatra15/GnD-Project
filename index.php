35be853a4e430ee6107d290b4a914dc0c4083abd
fitraadinan <fanfitraadina@gmail.com>
2021-06-22 07:11:29 +0700

    New
    New

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Gnd Page!</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <input type="checkbox" id="check">
+    <label for="check">
+      <i id="btn"><img src="GND.png"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+    <img src="GND.png">
+    <ul>
+      <li><a href="#"><i class="fas fa-qrcode"></i>Dashboard</a></li>
+      <li><a href="#"><i class="fas fa-link"></i>Coba</a></li>
+      <li><a href="#"><i class="fas fa-stream"></i>HIHIHI</a></li>
+      <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
+      <li><a href="#"><i class="far fa-question-circle"></i>MUEHEHE</a></li>
+      <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
+      <li><a href="#"><i class="far fa-envelope"></i>HUBUNGI KAMI</a></li>
+    </ul>
+    </div>
+
+    <div class="navbar">
+      <ul>
+        <li><a class="active" href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+ <section></section>
+
+  </body>
+</html>
