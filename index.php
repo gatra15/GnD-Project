ded6d22a8f2679534299daca8e093b4f4978975d
fitraadinan <fanfitraadina@gmail.com>
2021-06-25 23:40:25 +0700

    revisi0

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    
     <title>Welcome to Gnd Page!</title>
+
+    <!-- Link CSS Local -->
+    <link href="GND.png" rel="icon">
     <link rel="stylesheet" href="style.css">
+
+    <!-- Script JS -->
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   </head>
   <body>
@@ -16,23 +22,29 @@
     <img src="GND.png">
     <ul>
       <li><a href="#"><i class="fas fa-qrcode"></i>Dashboard</a></li>
-      <li><a href="#"><i class="fas fa-link"></i>Coba</a></li>
-      <li><a href="#"><i class="fas fa-stream"></i>HIHIHI</a></li>
+      <li><a href="#"><i class="fas fa-link"></i>Shortcuts</a></li>
+      <li><a href="#"><i class="fas fa-stream"></i>Overview</a></li>
       <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
-      <li><a href="#"><i class="far fa-question-circle"></i>MUEHEHE</a></li>
       <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
-      <li><a href="#"><i class="far fa-envelope"></i>HUBUNGI KAMI</a></li>
     </ul>
     </div>
 
     <div class="navbar">
       <ul>
         <li><a class="active" href="#">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
       </ul>
     </div>
- <section></section>
+ <section>
+    <div class="container">
+      <p class="hi1">Hello,</p>
+      <p class="hi2">allow us to introduce ourselves!</p>
+      <p class="space"></p>
+      <p class="typewriter1">My Name is Galih Saputra</p>
+      <p class="typewriter2">My Name is Fitra Adina Nuzulia</p>
+    </div>
+ </section>
 
   </body>
 </html>
